0f6b68ebb1125cc2e670b5e8af7bb1ea3609d3a6
Kyojin Syo <kyojinsyo@gmail.com>
2017-11-22 13:42:02 +0900

    add two_layer.py, and batch_normalization option in new_MLP.py, and scaling function

--- two_layer.py
+++ two_layer.py
@@ -3,8 +3,8 @@ import numpy as np
 import tensorflow as tf
 import random
 
-class MLP:
-    def __init__(self, X, Y, X_val, Y_val, epochs = 100, hidden_size = 100, batch_size = 100, model_name = "test_model", activation = "sigmoid", batch_normalization = False):
+class two_layer:
+    def __init__(self, X, Y, X_val, Y_val, epochs = 100, hidden_1 = 100, hidden_2 = 100, batch_size = 100, model_name = "test_model", activation = "sigmoid"):
         # 学習データと検証用データに分けておく
         self.X = X # 入力
         self.Y = Y # 教師
@@ -15,38 +15,16 @@ class MLP:
         諸変数の設定
         '''
         self.input_layer_size = self.X.shape[1] #入力層の数
-        self.hidden_layer_size = hidden_size # 隠れ層の数、適当
+        self.hidden1_layer_size = hidden_1 # 隠れ層の数、適当
+        self.hidden2_layer_size = hidden_2 # 隠れ層の数、適当
         self.output_layer_size = self.Y.shape[1] #出力層の数
         self.batch_size = batch_size #バッチサイズ
         self.learning_rate = 0.01 # 学習率 適当
         self.epochs = epochs #エポック数
         self.activation = activation
-        self.batch_normalization = batch_normalization
         
         # 学習データの保存
         self.model_name = str(model_name)
-
-    def batch_norm_wrapper(self, inputs, is_training, decay = 0.999, epsilon=1e-5):
-        '''
-        バッチノーマライゼーション
-        '''
-        scale = tf.Variable(tf.ones([inputs.get_shape()[1]]))
-        beta = tf.Variable(tf.zeros([inputs.get_shape()[1]]))
-        pop_mean = tf.Variable(tf.zeros([inputs.get_shape()[1]]), trainable=False)
-        pop_var = tf.Variable(tf.ones([inputs.get_shape()[1]]), trainable=False)
-
-        if is_training:
-            batch_mean, batch_var = tf.nn.moments(inputs,[0])
-            train_mean = tf.assign(pop_mean,
-                                   pop_mean * decay + batch_mean * (1 - decay))
-            train_var = tf.assign(pop_var,
-                                  pop_var * decay + batch_var * (1 - decay))
-            with tf.control_dependencies([train_mean, train_var]):
-                return tf.nn.batch_normalization(inputs,
-                    batch_mean, batch_var, beta, scale, epsilon)
-        else:
-            return tf.nn.batch_normalization(inputs,
-            pop_mean, pop_var, beta, scale, epsilon)
         
         
     def shuffle(self):
@@ -59,36 +37,33 @@ class MLP:
         ys = np.array([self.Y[r] for r in idx])
         return xs, ys
         
-    def inference(self, input_ph, is_training):
+    def inference(self, input_ph):
         '''
         グラフの構築
         '''
         
         # 重みとバイアスの初期化
-        hidden_w = tf.Variable(tf.random_normal([self.input_layer_size, self.hidden_layer_size], stddev=0.01), name='hidden_w')
-        hidden_b = tf.Variable(tf.random_normal([self.hidden_layer_size]), name='hidden_b')
-        output_w = tf.Variable(tf.random_normal([self.hidden_layer_size, self.output_layer_size], stddev=0.01), name='output_w')
+        hidden1_w = tf.Variable(tf.random_normal([self.input_layer_size, self.hidden1_layer_size], stddev=0.01), name='hidden1_w')
+        hidden1_b = tf.Variable(tf.random_normal([self.hidden1_layer_size]), name='hidden1_b')
+        hidden2_w = tf.Variable(tf.random_normal([self.hidden1_layer_size, self.hidden2_layer_size], stddev=0.01), name='hidden2_w')
+        hidden2_b = tf.Variable(tf.random_normal([self.hidden2_layer_size]), name='hidden2_b')
+        output_w = tf.Variable(tf.random_normal([self.hidden2_layer_size, self.output_layer_size], stddev=0.01), name='output_w')
         output_b = tf.Variable(tf.random_normal([self.output_layer_size]), name='output_b')
-
-        if self.batch_normalization:
-            print("do batch normalization")
-            input_ph = self.batch_norm_wrapper(input_ph, is_training)
         
-
         # 計算
         if  self.activation == "sigmoid":
-            hidden = tf.sigmoid(tf.matmul(input_ph, hidden_w) + hidden_b)
+            hidden1 = tf.sigmoid(tf.matmul(input_ph, hidden1_w) + hidden1_b)
+            hidden2 = tf.sigmoid(tf.matmul(hidden1, hidden2_w) + hidden2_b)
         elif  self.activation == "relu":
-            hidden = tf.nn.relu(tf.matmul(input_ph, hidden_w) + hidden_b)
+            hidden1 = tf.nn.relu(tf.matmul(input_ph, hidden1_w) + hidden1_b)
+            hidden2 = tf.nn.relu(tf.matmul(hidden1, hidden2_w) + hidden2_b)
         elif self.activation == "tanh":
-            hidden = tf.tanh(tf.matmul(input_ph, hidden_w) + hidden_b)
+            hidden1 = tf.tanh(tf.matmul(input_ph, hidden1_w) + hidden1_b)
+            hidden2 = tf.tanh(tf.matmul(hidden1, hidden2_w) + hidden2_b)
 
-        if self.batch_normalization:
-            hidden = self.batch_norm_wrapper(hidden, is_training)
+        output = tf.matmul(hidden2, output_w) + output_b
 
-        output = tf.matmul(hidden, output_w) + output_b
-
-        weights = [hidden_w, output_w, hidden_w, hidden_b]
+        weights = [hidden1_w, hidden2_w, output_w, hidden1_b, hidden2_b, output_b]
         
         return output, weights
         
@@ -125,7 +100,7 @@ class MLP:
             input_ph = tf.placeholder('float', [None, self.input_layer_size], name='input')
             actual_ph = tf.placeholder('float', [None, self.output_layer_size], name='actual_value')
 
-            prediction, weights = self.inference(input_ph, is_training=True)
+            prediction, weights = self.inference(input_ph)
             cost = self.loss(prediction, actual_ph)
             optimizer = self.training(cost)
 
@@ -155,7 +130,7 @@ class MLP:
                     
                     sess.run(optimizer, feed_dict=train_dict)
 
-                    if (epoch) % (self.epochs//50) == 0:
+                    if (epoch) % (self.epochs//20) == 0:
                         val_dict = {
                             input_ph:      self.X_val,
                             actual_ph:     self.Y_val,
@@ -175,8 +150,6 @@ class MLP:
                     
                 # datas = sess.run(weights)
                 # saver.save(sess, "./data/model/" + str(self.model_name) + "/" + str(self.model_name) + ".ckpt")
-
-        return validation_losses[3:]
                 
     def predict(self, X_predict, model_name = "test_model"):
         '''
@@ -185,7 +158,7 @@ class MLP:
         # 予測に使う変数の用意
         tf.reset_default_graph()
         input_ph = tf.placeholder("float", [None, self.input_layer_size], name='input')
-        prediction, weights = self.inference(input_ph, is_training=False)
+        prediction, weights = self.inference(input_ph)
         pre_dict = {
             input_ph: X_predict,
         }
